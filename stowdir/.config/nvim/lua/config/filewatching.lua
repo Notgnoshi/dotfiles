48c297d7b332ea6d73b066fbe78139c9057d7e25
Austin Gill <Notgnoshi@gmail.com>
2026-05-03 14:24:44 -0500

    nvim: Detect file changes even when editor is not focused

--- stowdir/.config/nvim/lua/config/filewatching.lua
+++ stowdir/.config/nvim/lua/config/filewatching.lua
@@ -1,7 +1,14 @@
 vim.opt.autowrite = true
 vim.opt.updatetime = 1000
 
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
     group = vim.api.nvim_create_augroup("filewatching_checktime", { clear = true }),
     command = "checktime",
 })
+
+local checktime_timer = vim.uv.new_timer()
+checktime_timer:start(2000, 2000, vim.schedule_wrap(function()
+    if vim.fn.getcmdwintype() == "" then
+        vim.cmd("checktime")
+    end
+end))
