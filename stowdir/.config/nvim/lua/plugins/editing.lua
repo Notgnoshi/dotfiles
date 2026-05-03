5f5753dce031147e5344fedfe8ff4781a67730b1
Austin Gill <Notgnoshi@gmail.com>
2026-05-02 20:45:10 -0500

    nvim: Add flash.nvim

--- stowdir/.config/nvim/lua/plugins/editing.lua
+++ stowdir/.config/nvim/lua/plugins/editing.lua
@@ -9,4 +9,16 @@ return {
         "tpope/vim-eunuch",
         cmd = { "Move", "Rename", "Delete", "Chmod", "SudoWrite", "Mkdir", "Wall", "SudoEdit" },
     },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {
+            modes = {
+                char = {
+                    jump_labels = false,
+                },
+                search = { enabled = false },
+            },
+        },
+    },
 }
