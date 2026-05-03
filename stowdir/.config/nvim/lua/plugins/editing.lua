f76b105cf75cecf890a464b8a34bccda09be5cd0
Austin Gill <Notgnoshi@gmail.com>
2026-05-02 20:59:51 -0500

    nvim: Add vim-tmux-navigator

--- stowdir/.config/nvim/lua/plugins/editing.lua
+++ stowdir/.config/nvim/lua/plugins/editing.lua
@@ -26,4 +26,21 @@ return {
         event = "VeryLazy",
         opts = {},
     },
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<C-h>", "<cmd>TmuxNavigateLeft<cr>",     desc = "Tmux/win left" },
+            { "<C-j>", "<cmd>TmuxNavigateDown<cr>",     desc = "Tmux/win down" },
+            { "<C-k>", "<cmd>TmuxNavigateUp<cr>",       desc = "Tmux/win up" },
+            { "<C-l>", "<cmd>TmuxNavigateRight<cr>",    desc = "Tmux/win right" },
+            { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Tmux/win previous" },
+        },
+    },
 }
