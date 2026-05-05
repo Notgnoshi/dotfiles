a47ef29f74754cedbad363ab27ea38f6a15d7eea
Austin Gill <Notgnoshi@gmail.com>
2026-05-05 18:57:52 -0500

    nvim: Add diagnostic popup keybind

--- stowdir/.config/nvim/lua/config/diagnostics.lua
+++ stowdir/.config/nvim/lua/config/diagnostics.lua
@@ -1,5 +1,7 @@
 vim.diagnostic.config({
     virtual_text = { current_line = true },
     severity_sort = true,
-    float = { border = "none", source = true },
+    float = { border = "rounded", source = true },
 })
+
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
