6b2afa6d1fbf7f192cdcf7450c41a0f0ce1cbe73
Austin Gill <Notgnoshi@gmail.com>
2026-05-28 16:22:30 +0000

    setup: Create directories that shouldn't be symlinks before stowing

--- setup.d/010-update-dotfiles.sh
+++ setup.d/010-update-dotfiles.sh
@@ -6,6 +6,13 @@ if prompt_default_no "Update Git submodules?"; then
 fi
 
 if prompt_default_yes "Deploy dotfiles?"; then
+    # Stow will make these directories be symlinks if they don't exist ahead of time
+    mkdir -p ~/.bash_completion.d/
+    mkdir -p ~/.claude/hooks/
+    mkdir -p ~/.config/systemd/user/
+    mkdir -p ~/.copilot/
+    mkdir -p ~/.local/{bin,lib,share}/
+
     stow --verbose=1 --dir="$DOTFILES_SETUP_SCRIPT_DIR" --target="$HOME" --restow stowdir
     # Stow seems to ignore .gitignore, and I can't figure out how to force it, so do it by hand.
     if [[ ! -e "$HOME/.gitignore" ]]; then
